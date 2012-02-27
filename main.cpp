3462ef1fda11af8373e282d3fee404aefc346a5c
Eric Wieser <wieser.eric@gmail.com>
2012-02-27 11:15:01 +0000

    Removed silly thing, improved the wind vane stuff.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 
-
 #include "mbed.h"
 #include "SDFileSystem.h"
 
@@ -10,6 +9,10 @@
 
 //the definiton of what a counter is, as well as how to increace and reset it
 class Counter {
+private:
+    InterruptIn _interrupt;
+    volatile int _count;
+
 public:
     Counter(PinName pin) : _interrupt(pin) {        // create the InterruptIn on the pin specified to Counter
         _interrupt.rise(this, &Counter::increment); // attach increment function of this counter instance
@@ -22,21 +25,19 @@ public:
     int read() {
         return _count;
     }
+
+    int operator int() {
+        return read();
+    }
+
     void reset() {
         _count = 0;
     }
-
-
-
-private:
-    InterruptIn _interrupt;
-    volatile int _count;
 };
 
 //----------------------------------------
 
-DigitalOut lde1(LED1);
-AnalogIn In1 (p15);
+AnalogIn windVane(p15); //The thingy that does not spin, but points
 Serial pc(USBTX, USBRX);
 Timeout timeout;
 Counter WSCounter(p14);             //14
@@ -59,6 +60,8 @@ void reset_RN_value() {
 //DATA container
 char Buffer[1024];
 
+#define printToBuffer(...) writeToBuffer(__VA_ARGS__)
+
 //kep-press
 char c;
 
@@ -71,60 +74,59 @@ FILE* fp;
 
 //wind direction
 void Wind_dir () {
-    Voltage = In1;
-    printf(Buffer + strlen(Buffer), "Wind Direction ");
-    if ((In1 >= 0.9375)||(In1 < 0.0625)) {
-        printf(Buffer + strlen(Buffer), "North      at:");
+    float v = windVane * 16;
+    writeToBuffer("Wind Direction ");
+    if (v < 1 || v >= 15) {
+        writeToBuffer("North      at:");
     }
-    if ((In1 >= 0.0625)&&(In1 < 0.1875)) {
-        printf(Buffer + strlen(Buffer), "North East at:");
+    else if (v < 3) {
+        writeToBuffer("North East at:");
     }
-    if ((In1 >= 0.1875)&&(In1 < 0.3125)) {
-        printf(Buffer + strlen(Buffer), "East       at:");
+    else if (v < 5) {
+        writeToBuffer("East       at:");
     }
-    if ((In1 >= 0.341275)&&(In1 < 0.4375)) {
-        printf(Buffer + strlen(Buffer), "South East at:");
+    else if (v < 7) {
+        writeToBuffer("South East at:");
     }
-    if ((In1 >= 0.4375)&&(In1 < 0.5625)) {
-        printf(Buffer + strlen(Buffer), "South      at:");
+    else if (v < 9) {
+        writeToBuffer("South      at:");
     }
-    if ((In1 >= 0.5625)&&(In1 < 0.6875)) {
-        printf(Buffer + strlen(Buffer), "South West at:");
+    else if (v < 11) {
+        writeToBuffer("South West at:");
     }
-    if ((In1 >= 0.6875)&&(In1 < 0.8125)) {
-        printf(Buffer + strlen(Buffer), "West       at:");
+    else if (v < 13) {
+        writeToBuffer("West       at:");
     }
-    if ((In1 >= 0.8125)&&(In1 <= 0.9375)) {
-        printf(Buffer + strlen(Buffer), "North West at:");
+    else if (v < 15) {
+        writeToBuffer("North West at:");
     }
 }
 
 void WindSpeed() {
-    printf(Buffer + strlen(Buffer),"  %03dm/S  ", WSCounter.read());
+    writeToBuffer("  %03dm/s  ", WSCounter.read());
     WSCounter.reset();
 }
 
 void RainGauge() {
     float RainFall=0.0;
     RainFall = RFCounter.read() * 0.2;
-    printf(Buffer + strlen(Buffer),"Rain Fall per Hour: %03.1fmm ",RainFall);
+    writeToBuffer("Rain Fall per Hour: %03.1fmm ",RainFall);
 }
 
 
 void Humidity () {
     float humidity = 0.0;
     humidity = ((Humid * 3.3) -1.60)/0.0312;
-    printf(Buffer + strlen(Buffer),"Humidity: ");
-    printf(Buffer + strlen(Buffer), "%04.1f%s",humidity,"%% ");
+    writeToBuffer("Humidity: ");
+    writeToBuffer("%04.1f%s",humidity,"%% ");
 }
 
 
 void Temperature () {
     float Temperature1 = 0.0;
     Temperature1 = ((1000/Temp)-2005 )/4;
-    printf(Buffer + strlen(Buffer), "temperature is %02.0f°C   ", Temperature1);
+    writeToBuffer("temperature is %02.0f°C   ", Temperature1);
 }
-
 
 //Write data to log file on microSD card (i used the BOB V1.0)
 void Choice () {
@@ -137,7 +139,7 @@ void Choice () {
     do {
         FileName[N] = pc.getc();
         pc.printf("%c",FileName[N]);
-    } while ((FileName[N++] == '\n') | (N < 11));
+    } while (FileName[N++] != '\n' || N < 11);
     pc.printf("\n\r%s\n\r", &FileName[0]);
     printf(&FileName[--N],".txt");
     pc.printf("\n\r%s\n\r", &FileName[0]);
@@ -153,16 +155,17 @@ void Choice () {
     } else {
         fclose(fp);
         pc.printf("\r\n\r\n%s allready exists, do you want to: \n\r Append [A] \n\r Overwrite [O] \n\r or Create a new file [C]\n\r", &FileName[0]);
-        do {
-            c = pc.getc();
-        } while ( !((c == 'A')||(c == 'a')||(c == 'O')||(c == 'o')) );
-        if ((c == 'A')||(c == 'a')) {
-            pc.printf("Appending %s", &FileName[0]);
-            fp = fopen(&FileName[0], "a");
-        }
-        if ((c == 'O') || (c == 'o')) {
-            pc.printf("Overwriting %s", &FileName[0]);
-            fp = fopen(&FileName[0], "w");
+        while(c = pc.getc())
+            if (c == 'A' || c == 'a') {
+                pc.printf("Appending %s", &FileName[0]);
+                fp = fopen(&FileName[0], "a");
+                break;
+            }
+            if (c == 'O' || c == 'o') {
+                pc.printf("Overwriting %s", &FileName[0]);
+                fp = fopen(&FileName[0], "w");
+                break;
+            }
         }
     }
     pc.printf("\n\rFile opened successfuly\n\r");
@@ -182,14 +185,14 @@ void Write () {
 
 int main() {
     Choice();
-    printf(Buffer + strlen(Buffer), "\r\n ----- M-BED Weather Monitoring -----\r\n\r\n");
+    writeToBuffer("\r\n ----- M-BED Weather Monitoring -----\r\n\r\n");
     while (1) {
         Wind_dir();
         WindSpeed();
         RainGauge();
         Humidity();
         Temperature();
-        printf(Buffer + strlen(Buffer), "\r\n-----\r\n");
+        writeToBuffer("\r\n-----\r\n");
         Write();
         wait(1.0);
     }
